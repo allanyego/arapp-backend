fb062fb6a83f5d5c52f27a6dc7c39a7e6b6caa0e
yegow <allanyego05@gmail.com>
2021-03-22 07:39:59 +0300

    Fix guide posting

--- joi-schemas/guide.js
+++ joi-schemas/guide.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const newSchema = Joi.object({
   title: Joi.string().required().trim(),
   body: Joi.string().required(),
-  links: Joi.string(),
+  links: Joi.string().allow(""),
 });
 
 const voteSchema = Joi.object({
